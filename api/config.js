3e3eb522435e9d7437e7da09d213dcf740aafa91
Gui Nascimento <55475010+guibyui@users.noreply.github.com>
2021-04-06 14:47:40 -0600

    Appointment Booking app

---
+++ api/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  dbHost: 'localhost',
+  dbName: 'appointments-app',
+  dbCollection: 'appointments',
+};
+
+
---
+++ app/src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { AppointmentService } from '../appointment.service';
+import { Appointment } from '../Appointment';
+import { mergeMap } from 'rxjs/operators';
+
+@Component({
+  selector: "app-appointment-list",
+  templateUrl: "./appointment-list.component.html",
+  styleUrls: ["./appointment-list.component.css"],
+})
+export class AppointmentListComponent implements OnInit {
+  public loading = true;
+  public errorMsg: string;
+  public successMsg: string;
+  public appointments: Appointment[];
+  public columns = ["appointmentDate", "name", "email", "cancel", "update"];
+
+  constructor(private appointmentService: AppointmentService) {}
+
+  ngOnInit() {
+    this.appointmentService.getAppointments().subscribe(
+      (appointments: Appointment[]) => {
+        this.appointments = appointments;
+        this.loading = false;
+      },
+      (error: ErrorEvent) => {
+        this.errorMsg = error.error.message;
+        this.loading = false;
+      }
+    );
+  }
+
+  cancelAppointment(id: string) {
+    this.appointmentService
+      .cancelAppointment(id)
+      .pipe(mergeMap(() => this.appointmentService.getAppointments()))
+      .subscribe(
+        (appointments: Appointment[]) => {
+          this.appointments = appointments;
+          this.successMsg = "Successfully cancelled appointment";
+        },
+        (error: ErrorEvent) => {
+          this.errorMsg = error.error.message;
+        }
+      );
+  }
+
+  updateAppointment(id: string) {
+    this.appointmentService
+      .updateAppointment(id)
+      .pipe(mergeMap(() => this.appointmentService.getAppointments()))
+      .subscribe(
+        (appointments: Appointment[]) => {
+          this.appointments = appointments;
+          this.successMsg = "Successfully updated appointment";
+        },
+        (error: ErrorEvent) => {
+          this.errorMsg = error.error.message;
+        }
+      );
+  }
+}
